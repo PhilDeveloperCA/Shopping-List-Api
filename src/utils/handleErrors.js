7821225efe0b91ebcd90d5026da508c821aa721a
Pranosaurus <padler@uci.edu>
2021-08-22 23:36:44 -0700

    Vulnerability Cover Logs

--- src/utils/handleErrors.js
+++ src/utils/handleErrors.js
@@ -11,7 +11,7 @@ const handleErrors = (err,req,res,next) => {
     
     return res.status(500).json({
         status : 'error',
-        message : err.message
+        message : "System Error"
     })
 }
 
